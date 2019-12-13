7d4eba247f45e6c762042ab0ff0b659450bdcce4
Roberto Limón <lrlimon@gmail.com>
2019-12-12 22:48:05 -0600

    Add index to Label controller.

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,11 @@
 class LabelsController < ApplicationController
+  def get_labels(source = nil)
+    source = "data/labels.json" if source == nil
+
+    return File.file?(source) ? JSON.parse(File.read(source), object_class: OpenStruct) : []
+  end
+
+  def index
+    @labels = get_labels
+  end
 end
